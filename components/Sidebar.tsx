da176216585f84654f76ff2a383408be1e0d30ed
AvionShea <avionsheadesigns@gmail.com>
2024-05-18 21:39:13 -0400

    create sidebar links

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,7 +1,14 @@
+"use client";
+
 import Link from 'next/link'
 import Image from 'next/image'
+import { sidebarLinks } from '@/constants'
+import { cn } from '@/lib/utils'
+import { usePathname } from 'next/navigation'
 
 const Sidebar = ({user}: SiderbarProps) => {
+    const pathName = usePathname();
+
   return (
     <section className='sidebar'>
         <nav className='flex flex-col gap-4'>
@@ -17,6 +24,16 @@ const Sidebar = ({user}: SiderbarProps) => {
                     Lotus Bank
                 </h1>
             </Link>
+
+            {sidebarLinks.map((item) => {
+                const isActive = pathName === item.route || pathName.startsWith(`${item.route}/`)
+
+                return (
+                    <Link href={item.route} key={item.label} className={cn("sidebar-link", {"bg-bank-gradient": isActive})}>
+                        {item.label}
+                    </Link>
+                )
+            })}
         </nav>
     </section>
   )
