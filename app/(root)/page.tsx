dc9c23ffb22a73495b7a8ecb6dd63c95fb28c10e
AvionShea <avionsheadesigns@gmail.com>
2024-05-27 00:27:37 -0400

    implement log off function and added img to sign-in/sign-up page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,35 +1,36 @@
 import HeaderBox from '@/components/HeaderBox';
 import RightSidebar from '@/components/RightSidebar';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
+import { getLoggedInUser } from '@/lib/actions/user.actions';
 
-const Dashboard = () => {
-  const loggedIn = {firstName: "Avion", lastName: "Cobb", email: "firstNameLastName@email.com"};
+const Dashboard = async () => {
+  const loggedIn = await getLoggedInUser();
   return (
     <section className="dashboard">
       <div className='dashboard-content'>
         <header className='dashboard-header'>
-            <HeaderBox
-              type="greeting"
-              title="Welcome,"
-              user= {loggedIn?.firstName + "!" || "Guest!"}
-              subtext = "Access and manage your account and transactions efficiently."
-            />
-            <TotalBalanceBox
-              accounts = {[]}
-              totalBanks = {3}
-              totalCurrentBalance = {134986.23}
-            />
+          <HeaderBox
+            type="greeting"
+            title="Welcome,"
+            user={loggedIn?.name + "!" || "Guest!"}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={3}
+            totalCurrentBalance={134986.23}
+          />
         </header>
 
         RECENT TRANSACTIONS
 
       </div>
 
-        <RightSidebar
-          user={loggedIn}
-          transactions={[]}
-          banks={[{}, {}]}
-        />
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{}, {}]}
+      />
 
     </section>
   )
