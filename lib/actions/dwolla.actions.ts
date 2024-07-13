ac0f41e9972834d2e8b95fcd64c6d1f92c2c1846
AvionShea <avionsheadesigns@gmail.com>
2024-07-13 03:22:51 -0400

    package changes

--- lib/actions/dwolla.actions.ts
+++ lib/actions/dwolla.actions.ts
@@ -3,11 +3,13 @@
 import { Client } from "dwolla-v2";
 
 const getEnvironment = (): "sandbox" | "production" => {
-  const environment = "sandbox";
+  const environment = process.env.DWOLLA_ENV as string;
 
   switch (environment) {
     case "sandbox":
       return "sandbox";
+    case "production":
+      return "production";
     default:
       throw new Error(
         "Dwolla environment should either be set to `sandbox` or `production`"
