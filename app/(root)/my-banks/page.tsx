f86e790ccd691f79524dc3b833fab4f6a5a7748f
AvionShea <avionsheadesigns@gmail.com>
2024-07-13 03:11:47 -0400

    code change

--- app/(root)/my-banks/page.tsx
+++ app/(root)/my-banks/page.tsx
@@ -6,19 +6,23 @@ import React from 'react'
 
 const MyBanks = async () => {
   const loggedIn = await getLoggedInUser();
-  const accounts = await getAccounts({ userId: loggedIn.$id });
+  const accounts = await getAccounts({
+    userId: loggedIn.$id
+  })
 
   return (
     <section className='flex'>
-      <div className='my-banks'>
+      <div className="my-banks">
         <HeaderBox
-          title='My Bank Accounts'
-          subtext='Effortlessly manage your banking activities.'
+          title="My Bank Accounts"
+          subtext="Effortlessly manage your banking activities."
         />
 
-        <div className='space-y-4'>
-          <h2 className='header-2'>Current accounts</h2>
-          <div className='flex flex-wrap gap-6'>
+        <div className="space-y-4">
+          <h2 className="header-2">
+            Your cards
+          </h2>
+          <div className="flex flex-wrap gap-6">
             {accounts && accounts.data.map((a: Account) => (
               <BankCard
                 key={accounts.id}
@@ -28,7 +32,6 @@ const MyBanks = async () => {
             ))}
           </div>
         </div>
-
       </div>
     </section>
   )
